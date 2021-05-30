8fd48711b5d560a9445248d64e386149312dd35e
minsekim <minsekim@minsekimui-Macmini.local>
2021-05-31 07:01:05 +0900

    defalut campustaxi nodejs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       wait_ready: true,
       listen_timeout: 10000,
       kill_timeout: 5000,
-      // max_memory_restart: "500M",
+      max_memory_restart: "2G",
       env: {
         NODE_ENV: "development",
       },
