b6099db3be085919407112c6375928cbf596436f
Mins97 <tkarnrwl7862@naver.com>
2021-06-03 15:31:47 +0900

    defalut campustaxi nodejs

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const express = require("express");
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
-const port = 3100;
+const port = 3000;
 import {} from "./premium/premium";
 import { logger } from "./config/winston";
 
